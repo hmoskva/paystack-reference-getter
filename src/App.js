529e5f4cf05ed44ebab5d1e7c7030620183113bf
Habib Sogbesan <h.d.sogbesan@gmail.com>
2023-01-31 13:07:22 +0100

    + code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { PaystackButton } from "react-paystack";
 
 function App() {
+  const config = {
+    reference: new Date().getTime().toString(),
+    email: "h.d.sogbesan@gmail.com",
+    amount: 200000, //Amount is in the country's lowest currency. E.g Kobo, so 20000 kobo = N200
+    publicKey: "pk_test_7ff4fce15eed96e8ab32929f57eba26532326b87", // replace with the public key thats connected to the environment youre targetting
+  };
+
+  const handlePaystackSuccessAction = (reference) => {
+    // Implementation for whatever you want to do with reference and after success call.
+    console.log("copy this reference", reference);
+  };
+
+  // you can call this function anything
+  const handlePaystackCloseAction = () => {
+    // implementation for  whatever you want to do when the Paystack dialog closed.
+    console.log("closed");
+  };
+
+  const componentProps = {
+    ...config,
+    text: "Paystack Button Implementation",
+    onSuccess: (reference) => handlePaystackSuccessAction(reference),
+    onClose: handlePaystackCloseAction,
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PaystackButton {...componentProps} />
     </div>
   );
 }
